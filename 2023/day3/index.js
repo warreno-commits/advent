e542bf7f1f14ea1f28793a6ea2ce1b6f5a9796df
Warren Overmyer <warrenovermyer@gmail.com>
2023-12-05 20:31:31 -0800

    test

--- 2023/day3/index.js
+++ 2023/day3/index.js
@@ -5,13 +5,10 @@ const lines = fs.readFileSync('./example.txt', 'utf-8').trim().split('\n')
 // console.log(lines.length)
 function partOne(){
     const partNumbers = [];
-    for (let i = 0 ; i < lines.length; i++) {
+    for (let i = 0 ; i < 1; i++) {
         console.log(lines[i].replace(/\D/g,' ').split(" "))
-        partNumbers.push(parseInt(lines[i].replace(/\D/g,' ').split(" ")))
+      //  partNumbers.push(parseInt(lines[i].replace(/\D/g,' ').split(" ")))
         }
-   // console.log(partNumbers)    
 }
-
-console.log(lines);
 
 partOne();
