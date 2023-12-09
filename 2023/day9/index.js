2ad1c681fc2a984b50c00a27d3f560d8f6e9d223
Warren <warrenovermyer@gmail.com>
2023-12-09 10:37:05 -0800

    History Sum works

--- 2023/day9/index.js
+++ 2023/day9/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const lines = fs.readFileSync('./example.txt', 'utf-8').trim().split('\n')
-console.log(lines);
+// console.log(lines);
 
 function processHistory(history){
 let historyArray = []
@@ -20,21 +20,30 @@ while(findZero != 0){
                 previousValue = currentLineOfHistory[i]
                 diffValues.push(currentDiff);
          }
-    console.log(historyArray);
+   // console.log(historyArray);
     historyArray.push(diffValues);
-    console.log("diffValues" + diffValues)
+  //  console.log("diffValues" + diffValues)
     currentLineOfHistory = diffValues
     currentParse++
     if(diffValues[0] == 0 && diffValues[1] == 0) {
-        console.log("Found Zero")
+     //   console.log("Found Zero")
         findZero = 0
     }
 }
-console.log(historyArray);
+// console.log(historyArray);
+let historySum = 0
+for(let j = historyArray.length-1; j >= 0  ; j--){
+let currentArray = historyArray[j]
+// console.log(currentArray)
+// console.log(currentArray[currentArray.length-1])
+historySum = historySum + parseInt(currentArray[currentArray.length-1])
+console.log("historySum " + historySum)
+}
+
 }
 
 function partOne(){
-    for (let i = 0 ; i < lines.length; i++){
+    for (let i = 0 ; i < 1; i++){
     let lineArray = lines[i].replace("\r","").split(" ")
     processHistory(lineArray);
     }
