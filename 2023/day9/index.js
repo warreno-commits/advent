6c2f6e85de213b71a30a1f14bbd36c9dfa39e559
Warren <warrenovermyer@gmail.com>
2023-12-09 10:40:45 -0800

    Part 1 solved

--- 2023/day9/index.js
+++ 2023/day9/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const lines = fs.readFileSync('./example.txt', 'utf-8').trim().split('\n')
+const lines = fs.readFileSync('./input.txt', 'utf-8').trim().split('\n')
 // console.log(lines);
 
 function processHistory(history){
@@ -39,14 +39,17 @@ let currentArray = historyArray[j]
 historySum = historySum + parseInt(currentArray[currentArray.length-1])
 console.log("historySum " + historySum)
 }
-
+return historySum
 }
 
 function partOne(){
-    for (let i = 0 ; i < 1; i++){
+let runningHistoryTotal = 0
+    for (let i = 0 ; i < lines.length; i++){
     let lineArray = lines[i].replace("\r","").split(" ")
-    processHistory(lineArray);
+    let historyValue = processHistory(lineArray);
+    runningHistoryTotal = runningHistoryTotal + historyValue
     }
+    console.log(runningHistoryTotal)
 }
 
 partOne();
